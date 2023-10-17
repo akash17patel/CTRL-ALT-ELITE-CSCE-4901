d182888abaab01d8e5054d84a8a56016b5ecc030
Akash Patel <akashpatelsa@gmail.com>
2023-10-16 23:06:06 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  //Root of application
+  // Root of the application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MindLift Demo',
       theme: ThemeData(
+        primaryColor: Color.fromRGBO(128, 32, 217, 1.0),
         colorScheme:
             ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 0, 0, 0)),
         appBarTheme: AppBarTheme(
-          color: Color.fromARGB(210, 28, 182, 213),
+          color: Color.fromRGBO(128, 32, 217, 1.0),
         ),
         useMaterial3: true,
       ),
@@ -30,7 +31,8 @@ class HomeTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-          'Home Tab Content'), // Replace this with your actual home tab content
+        'Home Tab Content', // Replace this with your actual home tab content
+      ),
     );
   }
 }
@@ -38,43 +40,89 @@ class HomeTab extends StatelessWidget {
 class HomeScreen extends StatefulWidget {
   final String title;
 
-  const HomeScreen({super.key, required this.title});
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2, //Number of tabs
+      length: 2, // Number of tabs
       child: Scaffold(
+        key: _scaffoldKey, // Attaching the GlobalKey to the Scaffold
         appBar: AppBar(
           title: Center(
-            child: Text('MindLift',
-                style: TextStyle(
-                  fontFamily: 'Merriweather-Bold',
-                  fontSize: 25.0,
-                  color: const Color.fromARGB(255, 0, 0, 0),
-                  fontWeight: FontWeight.bold,
-                )),
+            child: Text(
+              'MindLift',
+              style: TextStyle(
+                fontFamily: 'Merriweather-Bold',
+                fontSize: 25.0,
+                color: const Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.home), text: 'Home'),
-              Tab(icon: Icon(Icons.settings), text: 'Settings'),
-            ],
-            indicatorColor: Color.fromARGB(255, 0, 0, 0),
-            unselectedLabelColor: Color.fromARGB(255, 87, 87, 87),
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            HomeTab(), //Home Tab content (if any)
-            SettingsTab(), //Settings tab content
+          actions: [
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                // Open the endDrawer (SettingsDrawer)
+                _scaffoldKey.currentState?.openEndDrawer();
+              },
+            ),
           ],
         ),
+        endDrawer: SettingsDrawer(), // Add the SettingsDrawer to the endDrawer
+        body: TabBarView(
+          children: [
+            HomeTab(), // Home Tab content (if any)
+            SettingsTab(), // Settings tab content
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 150,
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Text(
+                'Settings',
+                style: TextStyle(
+                  fontFamily: 'Merriweather-LightItalic',
+                  fontSize: 35,
+                  color: Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text('My Profile'),
+            onTap: () {
+              // Implement the action for Option 1, e.g., navigate to a new page
+            },
+          ),
+          ListTile(
+            title: Text('Password Reset'),
+            onTap: () {
+              // Implement the action for Option 2, e.g., navigate to a different page
+            },
+          ),
+          // Add more ListTile items for additional options
+        ],
       ),
     );
   }
@@ -86,22 +134,6 @@ class SettingsTab extends StatefulWidget {
 }
 
 class _SettingsTabState extends State<SettingsTab> {
-  String _email = '';
-  String _message = ''; //Variable to store the message
-
-  void _resetPassword() {
-    if (_email.contains('@')) {
-      setState(() {
-        _message = 'Sent'; //Email is valid, set the message to 'Sent'
-      });
-    } else {
-      setState(() {
-        _message =
-            'Email Invalid'; //Email invalid, set message to 'Email Invalid'
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -109,31 +141,21 @@ class _SettingsTabState extends State<SettingsTab> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          TextField(
-            onChanged: (value) {
-              setState(() {
-                _email = value;
-              });
+          PopupMenuButton<String>(
+            onSelected: (String choice) {
+              if (choice == 'Open New Tab') {
+                DefaultTabController.of(context)
+                    ?.animateTo(1); // Change to the index of your new tab
+              }
             },
-            decoration: InputDecoration(
-              labelText: 'Email',
-              hintText: 'Enter your email',
-            ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              _resetPassword();
+            itemBuilder: (BuildContext context) {
+              return <PopupMenuEntry<String>>[
+                PopupMenuItem<String>(
+                  value: 'Open New Tab',
+                  child: Text('Open New Tab'),
+                ),
+              ];
             },
-            child: Text('Reset Password'),
-          ),
-          SizedBox(height: 20),
-          Text(
-            _message, // Display message based on email validation
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.red,
-            ),
           ),
         ],
       ),
