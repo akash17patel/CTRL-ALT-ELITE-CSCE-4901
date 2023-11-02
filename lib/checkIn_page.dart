19da914d063f12abb1ee7e666239612f6a395360
Shawana6697 <shawanatahseen@my.unt.edu>
2023-11-02 16:24:39 -0500

    check in and confirmation pop uo

---
+++ lib/checkIn_page.dart
@@ -0,0 +1,86 @@
+
+import 'package:flutter/material.dart';
+
+class CheckInPage extends StatefulWidget {
+  //const CheckInPage({super.key});
+  final String title;
+
+  const CheckInPage({super.key, required this.title});
+
+  @override
+  State<CheckInPage> createState() => _CheckInPageState();
+}
+
+class _CheckInPageState extends State<CheckInPage> {
+  
+  bool isEmotionOpened = false;
+  bool isNotesOpened = false;
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('Check In Page'),
+        centerTitle: true,
+      ),
+      body:Container(
+        height: double.infinity, width: double.infinity,color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+            const SizedBox(height:200),
+           InkWell(
+            onTap:(){
+              print('12345');
+              setState(() {
+                isEmotionOpened = !isEmotionOpened;
+                
+              });
+              
+            },
+            child: Container(height:60,width:200,color: Colors.grey, child:Center(child: Text('Emotion Selection')))),
+           SizedBox(height: 15,),
+           
+          isEmotionOpened == true? TextField():SizedBox(),
+           SizedBox(height: 15,),
+        
+           GestureDetector(
+            onTap: () {
+              print ('1223445');
+              setState(() {
+                isNotesOpened = !isNotesOpened;
+        
+              });
+              
+            },
+            child: Container(height:60,width:200,color: Colors.grey, child:Center(child: Text('Notes/Comments')))),
+            SizedBox(height: 15,),
+           
+          isNotesOpened == true? TextField():SizedBox(),
+           SizedBox(height: 50,),
+        
+           InkWell(
+            onTap: () {
+              showDialog(context: context, builder: (context){
+                return AlertDialog(title: Text('Are You Sure?'),content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  
+                  children: [
+                  ElevatedButton(onPressed: (){}, child: Text('Yes')),
+                   ElevatedButton(onPressed: (){}, child: Text('No')),
+                ]),);
+              });
+            },
+            
+            child: Container(height:60,width:200,color: Colors.red, child:Center(child: Text('Confirm'))))
+        
+          
+          ],),
+        ),
+      ) ,
+    );
+  }
+}
